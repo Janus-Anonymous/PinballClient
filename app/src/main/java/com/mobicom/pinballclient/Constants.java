0eeefe474cb912b70b1d81795a54b53287dc82ae
PinBall-Anonymous <xiazenghua@gmail.com>
2016-03-30 08:53:45 +0800

    Update Constants.java

--- app/src/main/java/com/mobicom/pinballclient/Constants.java
+++ app/src/main/java/com/mobicom/pinballclient/Constants.java
@@ -1,8 +1,5 @@
 package com.mobicom.pinballclient;
 
-/**
- * Created by JYM on 2016/3/28.
- */
 public class Constants {
     public static final String JSON_DEFAULT_URL = "http://121.42.158.232/json_test.txt";
     public static final String IMAGE_DEFAULT_URL = "https://avatars0.githubusercontent.com/u/17853516?v=3&s=460";
